6ab8a5064595ee0b957fe29eb8ab15a89195ebc5
Rodrigo Cruz <cruz.jsrodrigo@gmail.com>
2023-03-22 14:20:25 -0300

    DBContext configuration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using CatalogApi.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+    );
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
