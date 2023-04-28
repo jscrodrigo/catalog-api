2d8f72344c6246b5fa8ba2a239c67ff89911889c
Rodrigo Cruz <cruz.jsrodrigo@gmail.com>
2023-04-28 13:55:58 -0300

    Creation of the basic methods

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using CatalogApi.Context;
 using CatalogApi.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatalogApi.Controllers
 {
@@ -27,7 +29,7 @@ namespace CatalogApi.Controllers
             return products;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name="GetProduct")]
         public ActionResult<Product> GetById(int id)
         {
             var product = _context.Products.FirstOrDefault(p => p.Id == id);
@@ -38,6 +40,56 @@ namespace CatalogApi.Controllers
             }
 
             return product;
+        }
+
+        [HttpPost]
+        public ActionResult Include(Product product)
+        {
+            if (null == product)
+            {
+                return BadRequest("Body must be informed!");
+            }
+
+            _context.Products?.Add(product);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult(
+                "GetProduct",
+                new
+                {
+                    id = product.Id
+                },
+                product);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest("The IDs must be the same!");
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult DeleteById(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+
+            if (null == product)
+            {
+                return NotFound("The specified product was not found.");
+            }
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return Ok(product);
         }
     }
 }
